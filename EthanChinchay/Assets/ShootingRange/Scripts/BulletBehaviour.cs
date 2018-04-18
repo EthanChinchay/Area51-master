7275da4100804ce75a84fd65ef11fc07c2e55d16
Ethan Chinchay <ethanchinchay@hotmail.com>
2018-04-17 21:59:09 -0500

    3d shooter

---
+++ EthanChinchay/Assets/ShootingRange/Scripts/BulletBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour {
+
+    public float speed = 10;
+    public float lifeSpan = 3;
+
+	// Use this for initialization
+	void Start () {
+
+        Destroy(gameObject, 2);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(transform.forward * speed * Time.deltaTime);
+
+	}
+}
